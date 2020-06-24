c141665fd072162504b33fb22bb053de14e667e5
Jay Barr <jayrbarr@gmail.com>
2020-06-24 19:33:08 -0400

    Adds user authentication, sign up, sign in through JWTs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const jwt = require('jsonwebtoken');
 
 const db = require('./db');
 const models = require('./models');
@@ -11,6 +12,16 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
 
+const getUser = token => {
+  if (token) {
+    try {
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      throw new Error('Session invalid');
+    }
+  }
+};
+
 db.connect(DB_HOST);
 
 app.get('/', (req, res) => res.send('Hello World!!!'));
@@ -18,7 +29,12 @@ app.get('/', (req, res) => res.send('Hello World!!!'));
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => ({ models }),
+  context: ({ req }) => {
+    const token = req.headers.authorization;
+    const user = getUser(token);
+    console.log(user);
+    return { models, user };
+  },
 });
 
 server.applyMiddleware({ app, path: '/api' });
